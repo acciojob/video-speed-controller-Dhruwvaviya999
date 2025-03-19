988e66b1c4535d8fc972bf4ef62ed592d53cc0a0
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-19 15:01:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,5 +5,18 @@ const inputs = document.querySelectorAll('.controls input');
       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     }
 
+let video = document.querySelector(".player__video");
+let toggleBtn = document.querySelector(".player_button");
+
+	function pausePlay(){
+		if(video.paused){
+			video.play();
+			toggleBtn.value = "❚❚";
+		}else {
+			video.pause();
+			toggleBtn.value = "▶";
+		}
+	}
+
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
